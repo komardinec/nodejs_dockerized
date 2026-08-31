e426ada54f71374ef0f9527c5578015032fbbeb9
Vyacheslav Ter Avest <suicidemindv@gmail.com>
2026-08-31 06:30:45 +0300

    Added separate workflow for Terraform

--- terraform/config.tf
+++ terraform/config.tf
@@ -7,8 +7,10 @@ terraform {
   }
 }
 
-provider "google" {
-  project = "test-project-499111"
-  region = "europe-central2"
-  zone = "europe-central2-a"
+provider "google" { 
+  credentials = file("./service_account_file.json") # file will be created inside runner based on repository variable GCP_SERVICE_ACCOUNT_CONTENTS
+  # containing service admin variables
+  #project = "example-project-123456" # declared by env variable GOOGLE_PROJECT 
+  #region = "" # declared by env variable GOOGLE_REGION
+  #zone = "" # declared by env variable GOOGLE_ZONE 
 }
